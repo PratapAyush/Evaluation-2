12c7ef637a60a959e7f6c9533903a2b0f18691c4
PratapAyush <ayushpratapsingh235@gmail.com>
2022-05-30 16:58:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import AddProduct from "./components/AddProduct"
+import Pagination from "./components/Pagination"
+import Product from "./components/Product"
 
 const App = () => {
   return <div>{/* TODO: Code here */}</div>;
